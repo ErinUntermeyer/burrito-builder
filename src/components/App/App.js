41d661339b39d755aa713dd9dde2a2ef3b90f467
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-22 09:20:30 -0600

    Add state, setState with fetched data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,12 +5,18 @@ import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
 class App extends Component {
-  constructor(props) {
-    super();
+  constructor() {
+		super();
+		this.state = {
+			orders: []
+		}
   }
 
   componentDidMount() {
-    getOrders()
+		getOrders()
+			.then(data => {
+				this.setState({ orders: data.orders })
+			})
       .catch(err => console.error('Error fetching:', err));
   }
 
@@ -21,7 +27,6 @@ class App extends Component {
           <h1>Burrito Builder</h1>
           <OrderForm />
         </header>
-
         <Orders orders={this.state.orders}/>
       </main>
     );
