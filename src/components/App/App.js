7dcca84734ba07d562d9132cc8876d4f8fdc3c60
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-22 11:30:32 -0600

    Write remove order method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-import { getOrders, postOrder } from '../../apiCalls'
+import { getOrders, postOrder, deleteOrder } from '../../apiCalls'
 import Orders from '../../components/Orders/Orders'
 import OrderForm from '../../components/OrderForm/OrderForm'
 
@@ -29,6 +29,13 @@ class App extends Component {
 			.catch(err => console.error('Error fetching:', err))
 	}
 
+	removeOrder = id => {
+		deleteOrder(id)
+			.then(
+				this.setState({ orders: this.state.orders.filter(order => order.id !== id)})
+			)
+	}
+
   render() {
     return (
       <main className="App">
@@ -36,7 +43,7 @@ class App extends Component {
           <h1>Burrito Builder</h1>
           <OrderForm submitOrder={this.submitOrder}/>
         </header>
-        <Orders orders={this.state.orders}/>
+        <Orders orders={this.state.orders} removeOrder={this.removeOrder}/>
       </main>
     )
   }
