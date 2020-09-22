944f789194808e20a6fd0721933a31054600edf2
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-22 09:51:13 -0600

    Write submit order method called when submit button clicked

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-import {getOrders} from '../../apiCalls'
+import {getOrders, postOrder} from '../../apiCalls'
 import Orders from '../../components/Orders/Orders'
 import OrderForm from '../../components/OrderForm/OrderForm'
 
@@ -18,14 +18,22 @@ class App extends Component {
 				this.setState({ orders: data.orders })
 			})
       .catch(err => console.error('Error fetching:', err))
-  }
+	}
+	
+	submitOrder(name, ingredients) {
+		postOrder(name, ingredients)
+			.then(order => {
+				this.setState({ orders: [...this.state.orders, order] })
+			})
+			.catch(err => console.error('Error fetching:', err))
+	}
 
   render() {
     return (
       <main className="App">
         <header>
           <h1>Burrito Builder</h1>
-          <OrderForm />
+          <OrderForm submitOrder={this.submitOrder}/>
         </header>
         <Orders orders={this.state.orders}/>
       </main>
