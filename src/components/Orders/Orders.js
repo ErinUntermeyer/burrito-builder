b04980036f736df0c7688ea37a3c8e50c56ee1a8
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-22 09:20:50 -0600

    Add unique keys to each child element, destructure props

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Orders.css';
 
-const Orders = props => {
-  const orderEls = props.orders.map(order => {
+const Orders = ({ orders }) => {
+  const orderEls = orders.map(order => {
+		console.log(order)
     return (
-      <div className="order">
+      <div className="order" key={order.id}>
         <h3>{order.name}</h3>
         <ul className="ingredient-list">
           {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
+            return <li key={Math.random() * 5}>{ingredient}</li>
           })}
         </ul>
       </div>
